2d21ce8da03812923b90d3217eefe5db1f3b201d
Nick Ward <nickward13@hotmail.com>
2019-03-25 09:46:39 +1100

    added app insights

--- SimpleAspNetCoreWebApplication/Program.cs
+++ SimpleAspNetCoreWebApplication/Program.cs
@@ -19,6 +19,7 @@ namespace SimpleAspNetCoreWebApplication
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
