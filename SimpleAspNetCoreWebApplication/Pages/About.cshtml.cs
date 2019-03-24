c6f0afbaee56dc40350c3233b3a96acd26ec46dd
Nick Ward <nickward13@hotmail.com>
2019-03-25 09:34:41 +1100

    initial app

---
+++ SimpleAspNetCoreWebApplication/Pages/About.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleAspNetCoreWebApplication.Pages
+{
+    public class AboutModel : PageModel
+    {
+        [BindProperty]
+        public string CurrentOperatingSystem { get; set; }
+        [BindProperty]
+        public string CurrentIpAddress { get; set; }
+        [BindProperty]
+        public string CurrentHostName { get; set; }
+
+        public void OnGet()
+        {
+            CurrentOperatingSystem = Environment.OSVersion.VersionString;
+            CurrentHostName = System.Net.Dns.GetHostName();
+            CurrentIpAddress = System.Net.Dns.GetHostAddresses(CurrentHostName)[0].ToString();
+        }
+    }
+}
