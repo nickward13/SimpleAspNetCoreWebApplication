8516b6698d28351ac7816ab5cacaaa176f56f0b1
Nick Ward <nickward13@hotmail.com>
2019-07-18 12:28:03 +1000

    added host details to index page

--- SimpleAspNetCoreWebApplication/Pages/Index.cshtml.cs
+++ SimpleAspNetCoreWebApplication/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +10,17 @@ namespace SimpleAspNetCoreWebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string CurrentOperatingSystem { get; set; }
+        [BindProperty]
+        public string CurrentHostName { get; set; }
+        [BindProperty]
+        public IPAddress[] CurrentIpAddresses { get; set; }
         public void OnGet()
         {
-
+            CurrentOperatingSystem = Environment.OSVersion.VersionString;
+            CurrentHostName = System.Net.Dns.GetHostName();
+            CurrentIpAddresses = System.Net.Dns.GetHostAddresses(CurrentHostName);
         }
     }
 }
